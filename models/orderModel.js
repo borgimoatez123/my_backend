208f4eba401b23a52fad36496d78f0c32caa5290
borji moatez <123332288+borgimoatez123@users.noreply.github.com>
2024-05-03 17:22:35 -0700

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -35,3 +35,45 @@ const orderSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Order', orderSchema);
+/*const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({
+  cheeseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Cheese'
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1  // Ensures at least one item is ordered
+  }
+});
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  items: [orderItemSchema],  // Array of items
+  name: {
+    type: String,
+    required: false
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  address: {
+    type: String,
+    required: false
+  },
+  priceTotal: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
+*/
